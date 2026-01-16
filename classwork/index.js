cdbce98a4b0462e96f9f71e26ba43488f4a1ab7c
Mahak-Agrawal16 <agrawalmahak161204@gmail.com>
2026-01-16 05:45:47 +0530

    added info about code

---
+++ classwork/index.js
@@ -0,0 +1,23 @@
+//1 hello world
+console.log("Node.js Backend Started");
+
+//2 variables
+let name = "Ambesh";  //string
+let age = 22;        //number
+const country ="India";   //constant
+let isStudent = true;    //boolean
+
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(isStudent);
+
+//:- single line comment. used to explain code or disable execution of line
+//  console.log:- used to print the output in terminal or console
+//  let:- used to declare variable. values inside it can be changed.
+//  age:- age stores a number and it do not needs quotes.
+//  const:- declares a constant variable. values inside it can't be changed. must be initialized at declaration
+//  boolena variable:- represnts on/off, yes/no, 1/0
+//  all console.log():- prints the value stored in variables.
+//  quote:- the same value will be printed
+//  without quote:- value inside the var will be printed
